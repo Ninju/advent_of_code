bce94ddb59e4b5c4ce10fe878b0ed1748151706c
Ninju <alex.watt@rvu.co.uk>
2023-12-04 06:09:35 +0000

    2023 Day 4 - part 2 - solved

--- aoc_2023/day4/day4.lisp
+++ aoc_2023/day4/day4.lisp
@@ -5,13 +5,13 @@
 
 (defparameter example-input "/home/alex/src/workspace/advent_of_code/aoc_2023/day4/example.txt")
 (defparameter input "/home/alex/src/workspace/advent_of_code/aoc_2023/day4/input.txt")
-(defparameter *number-scanner* (ppcre:create-scanner "[0-9]+"))
-
 (defun get-main-lines ()
   (lib:read-file-lines input))
 
 (defun get-example-lines ()
   (lib:read-file-lines example-input))
+
+(defparameter *lines* (get-example-lines))
 
 
 (defparameter example-line "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")
@@ -40,12 +40,33 @@
     (lib:seq-intersection wins choices)))
 
 (defun scratchcard-value (scratchcard)
-  (let* ((winning (winning-numbers-in-card scratchcard))
-         (no-matches (length winning)))
-    (if (= no-matches 0)
-        0
-        (expt 2 (- no-matches 1)))))
+  (length (winning-numbers-in-card scratchcard)))
 
-(loop for line in (get-main-lines)
-      sum
-      (scratchcard-value (extract-card-details line)))
+(defun build-cards-db ()
+  (let ((cards (make-array (length *lines*))))
+    (loop for line in *lines*
+          do
+             (let* ((card (extract-card-details line))
+                    (card-no (parse-integer (car card)))
+                    (card-idx (- card-no 1)))
+               (setf (elt cards card-idx) card)))
+    cards))
+
+(defun main ()
+  (lib:sum (let ((cards-db (build-cards-db))
+                 (copies-db (make-array (length *lines*) :initial-element 1)))
+             (loop for scratchcard across cards-db
+                   do
+                      (let* ((wins (scratchcard-value scratchcard))
+                             (card-no (parse-integer (car scratchcard)))
+                             (number-copies-of-card (elt copies-db (- card-no 1))))
+                        (format t "~&Card ~d won ~d times (had ~d copies)~%" card-no wins number-copies-of-card)
+                        (loop for n from 1 to wins do
+                          (let ((newly-won-card-no (+ card-no n)))
+                            (if (not (> newly-won-card-no (length copies-db)))
+                                (setf (elt copies-db (+ card-no n -1))
+                                      (+ (* 1 number-copies-of-card) (elt copies-db (+ card-no n -1)))))))))
+             copies-db)))
+
+(let ((*lines* (get-main-lines)))
+  (main))
