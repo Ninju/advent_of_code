6b42244e4cf8dfd046a7174c190a23c2a1a429e5
Ninju <alex.watt@rvu.co.uk>
2023-12-04 05:31:08 +0000

    2023 Day 4 - Part 1 - solved

--- aoc_2023/day4/day4.lisp
+++ aoc_2023/day4/day4.lisp
@@ -3,8 +3,8 @@
 (ql:quickload :split-sequence)
 (ql:quickload :cl-ppcre)
 
-(defparameter example-input "/home/alex/src/workspace/advent_of_code/aoc_lang_2022/day4/example.txt")
-(defparameter input "/home/alex/src/workspace/advent_of_code/aoc_lang_2022/day4/input.txt")
+(defparameter example-input "/home/alex/src/workspace/advent_of_code/aoc_2023/day4/example.txt")
+(defparameter input "/home/alex/src/workspace/advent_of_code/aoc_2023/day4/input.txt")
 (defparameter *number-scanner* (ppcre:create-scanner "[0-9]+"))
 
 (defun get-main-lines ()
@@ -14,4 +14,38 @@
   (lib:read-file-lines example-input))
 
 
-(defparameter example-line "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
+(defparameter example-line "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")
+
+(defun extract-numbers-from-line (str)
+  (mapcar #'parse-integer
+          (ppcre:split "\\s+"
+                       (string-trim " " str))))
+
+(defun extract-winning-and-chosen-numbers (numbers-line)
+  (destructuring-bind (winning-numbers chosen-numbers)
+      (ppcre:split "\\|" numbers-line)
+    (mapcar #'extract-numbers-from-line
+            (list winning-numbers chosen-numbers))))
+
+(defun extract-card-details (card-line)
+  (ppcre:register-groups-bind (card-no numbers)
+      ("Card\\s+([0-9]+):(\.+)"
+       card-line)
+    (cons card-no
+          (extract-winning-and-chosen-numbers numbers))))
+
+(defun winning-numbers-in-card (scratchcard)
+  (destructuring-bind (card-no . (wins choices))
+      scratchcard
+    (lib:seq-intersection wins choices)))
+
+(defun scratchcard-value (scratchcard)
+  (let* ((winning (winning-numbers-in-card scratchcard))
+         (no-matches (length winning)))
+    (if (= no-matches 0)
+        0
+        (expt 2 (- no-matches 1)))))
+
+(loop for line in (get-main-lines)
+      sum
+      (scratchcard-value (extract-card-details line)))
