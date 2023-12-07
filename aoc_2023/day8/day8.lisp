d9eaa80c7ec73c2082e4bf651efff54a3b5231e2
Ninju <alex.watt@rvu.co.uk>
2023-12-07 17:05:01 +0000

    2023 Day 8 - get ready

---
+++ aoc_2023/day8/day8.lisp
@@ -0,0 +1,4 @@
+(in-package :aoc_2023)
+
+(defparameter example-input #p"/home/alex/src/workspace/advent_of_code/aoc_2023/day8/example.txt")
+(defparameter input #p"/home/alex/src/workspace/advent_of_code/aoc_2023/day8/input.txt")
