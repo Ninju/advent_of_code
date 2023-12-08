1c4126dd41cf7beebc3f3785aa05a91b0803a798
Ninju <alex.watt@rvu.co.uk>
2023-12-08 21:54:00 +0000

    2023 Day 8 - Part 2 - Solved

--- aoc_2023/day8/day8.lisp
+++ aoc_2023/day8/day8.lisp
@@ -38,6 +38,12 @@
           (incf (slot-value generator '_cursor)))
       next-element)))
 
+(defgeneric peek (generator))
+(defmethod peek ((generator circular-list))
+  (with-slots (_cursor _elements _size) generator
+    (let ((next-element (elt _elements _cursor)))
+      next-element)))
+
 (defun partition-by-equals (str)
   (ppcre:split map-entry-partition-regex str))
 
@@ -50,73 +56,38 @@
 
  (ppcre:all-matches-as-strings "\(([a-zA-Z]+)\\s*,\\s*([a-zA-Z]+)\)" "(BBB,CCC)")
 
-(defun make-map-key (move location)
-  (format nil "~a~a" move location))
-
-(defun getmap (map move location)
-  (let ((map-key (make-map-key move location)))
-    (gethash map-key map)))
-
-(defun setmap (map move location val)
-  (let ((map-key (make-map-key move location)))
-    (setf (gethash map-key map) val)))
-
-(defsetf getmap setmap)
-
 (defun lines->map-entries (lines)
   (let* ((hsh (make-hash-table :test #'equal))
          (map-entries (mapcar #'parse-map-entry-line lines)))
     (loop for entry in map-entries
           do
              (destructuring-bind (key left right) entry
-               (setf (getmap hsh left-move  key) left)
-               (setf (getmap hsh right-move key) right)
-               ))
+               (setf (gethash key hsh) (list left right))))
     hsh))
 
 (defmacro peek-move (starting-location move map)
-  `(gethash (make-map-key ,move ,starting-location) ,map))
-
-(defun _search-locations
-    (number-locations-searched pred map starting-locations visited-hsh)
-  (let ((locations
-          (arrows:-<>> starting-locations
-                      (remove-duplicates <> :test #'equal)
-                      (remove-if (lambda (loc) (gethash loc visited-hsh)))
-                      (remove-if pred))))
-    (if (= 0 (length locations))
-        number-locations-searched
-        (_search-max-depth-to-locations
-         (+ number-locations-searched (length locations))
-         pred
-         map
-         (mapcan (lambda (loc)
-                   (let ((left-loc  (peek-move loc left-move  map))
-                         (right-loc (peek-move loc right-move map)))
-                     (setf (gethash loc visited-hsh) T)
-                     (list left-loc right-loc)))
-                 locations)
-         visited-hsh))))
-
-(defun search-max-depth-to-locations (pred map starting-locations)
-  (let ((visited-hsh (make-hash-table :test #'equal)))
-    (_search-locations 0
-                       pred
-                       map
-                       starting-locations
-                       visited-hsh)))
+  (let ((g-exists (gensym))
+        (g-val (gensym)))
+  `(destructuring-bind (,g-val ,g-exists) ,(gethash starting-location map)
+     (if ,g-exists
+         (if (char= ,move ,left-move)
+             (car ,g-val)
+             (cadr ,g-val))))))
 
 (defun remove-first-char (str)
   (subseq str 1))
 
 (defun all-map-locations (map)
   (let ((keys (alexandria:hash-table-keys map)))
-    (remove-duplicates (mapcar #'remove-first-char keys)
+    (remove-duplicates keys
                        :test #'equal)))
 
-(defun string-ends-with-p (str char)
-  (char= (elt str (- (length str) 1))
+(defun location-ends-with-p (location-str char)
+  (char= (elt location-str 2)
          char))
+
+(defparameter *testing-moves* "LRLRRR")
+(defparameter *testing-circular-list* (make-instance 'circular-list :sequence *testing-moves*))
 
 ;; subseq 4   = 1322249
 ;; subseq 2 4 = 1024459
@@ -127,31 +98,71 @@
 ;; (require :sb-sprof)
 ;; (sb-sprof:start-profiling)
 
+(defun next-location (move map)
+  (lambda (loc)
+    (let ((nodes (gethash loc map)))
+      (if (equal current-move
+                 left-move)
+          (car nodes)
+          (cadr nodes)))))
 
-(time (destructuring-bind ((movements) map-entry-strings) (lib:read-file-line-groups input)
-  (let ((map (lines->map-entries map-entry-strings))
-        (moves (make-instance 'circular-list :sequence movements)))
-    (let ((starting-locations (subseq (remove-if-not (lambda (loc)
-                                               (string-ends-with-p loc #\A))
-                                             (all-map-locations map)) 0 2)))
+(defun get-left (map location)
+  (car (gethash location map)))
 
-    (loop
-      with current-locations = starting-locations
-      and current-move = (next moves)
-      and n = 0
-      until (every (lambda (loc) (string-ends-with-p loc #\Z)) current-locations)
-      count
-          (progn
-            ;; (format t "~&Step: ~d~%" (incf n))
-            (let ((next-locations (mapcar (lambda (loc)
-                                            (peek-move loc current-move map))
-                                  current-locations))
-                  (next-move (next moves)))
+(defun get-right (map location)
+  (cadr (gethash location map)))
 
-              ;; (format t "~2&CURRENT: ~a~%NEXT: ~a~2%" current-locations next-locations)
+(defun parse-moves-to-lambdas (moves)
+  (map 'vector
+       (lambda (c)
+         (if (char= c #\L)
+             #'get-left
+             (if (char= c #\R)
+                 #'get-right
+                 (error
+                  (format nil "Unexpected char: '~a', only 'L' and 'R' accepted" c)))))
+       moves))
 
-              (setf current-locations next-locations)
-              (setf current-move      next-move))))))))
+(defun step-through (map current-depth locations moves)
+  (let ((zs (remove-if-not (lambda (loc) (location-ends-with-p loc #\Z))
+                           locations)))
+    ;; (if (> (length zs) 0)
+    ;;     (format t
+    ;;             "~&Found ~d locations ending Z at depth ~d -- locations = ~a~%"
+    ;;             (length zs)
+    ;;             current-depth
+    ;;             locations))
+    (if (= (length locations) (length zs))
+        (progn
+          ;; (print locations)
+          ;; (print current-depth)
+          current-depth)
+        (let* ((next-move (next moves))
+               (new-locations (mapcar
+                               (lambda (loc) (funcall next-move map loc))
+                               locations)))
+          ;; (print next-move)
+          ;; (print new-locations)
+          (step-through map (+ 1 current-depth) new-locations moves)))))
+
+(time (let ((result
+        (destructuring-bind ((movements) map-entry-strings) (lib:read-file-line-groups input)
+          (let* ((map (lines->map-entries map-entry-strings))
+                 (moves (make-instance 'circular-list :sequence
+                                       (parse-moves-to-lambdas movements)))
+                 (starting-locations (remove-if-not (lambda (loc)
+                                                      (location-ends-with-p loc #\A))
+                                                    (all-map-locations map))))
+            (format t "Starting new run, starting at: ~a~%" starting-locations)
+            (mapcar (lambda (location)
+                      (step-through map 0 (list location) moves))
+                    starting-locations)))))
+        (format t "~2&The result is: ~d~%" (apply #'lcm result))))
+
+ ; => 14265111103729 (44 bits, #xCF95AE148F1)
+  ;   5,662,617,149
+
+
 
 
 ;; (sb-sprof:report :type :flat)
