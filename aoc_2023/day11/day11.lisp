d15cce3c229a9383f07df6dd7b05df8cf98c914c
Ninju <alex.watt@rvu.co.uk>
2023-12-11 12:13:30 +0000

    Day 11 - Part 2 - Solved

--- aoc_2023/day11/day11.lisp
+++ aoc_2023/day11/day11.lisp
@@ -9,6 +9,8 @@
 
 (defparameter *day-number* 11)
 
+(defparameter *universe-expansion-size* 1000000)
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun get-pathname (filename)
     (arrows:-> filename
@@ -33,7 +35,7 @@
 
 (defparameter *default-directions* (list *north* *south* *west* *east*))
 
-(defun not-implemented (&optional description)
+(defun not-implemented (&optional description &rest body)
   "Placeholder function for bits of code that aren't implemented yet"
   (error (format nil "Not implemented: ~a"
                  (if description description "no description given"))))
@@ -100,6 +102,17 @@
                                         (funcall predicate element)))))
                   (if (every #'identity truthies) x)))))
 
+(defun find-empty-columns (lines)
+  (find-columns lines #'space-p))
+
+(defun find-empty-rows (lines)
+  (remove nil
+          (loop for y from 0 below (length lines)
+                collect
+                (let ((line (elt lines y)))
+                  (if (every #'space-p line)
+                      y)))))
+
 (defun expand-universe (lines)
   (let* ((empty-columns (find-columns lines #'space-p))
          (expanded-rows (reduce (lambda (res line)
@@ -153,11 +166,83 @@
       (+ (abs (- x u))
          (abs (- y v))))))
 
-(let ((map (print (build-map *input*))))
+(defun pair-positions (positions)
+  (mapcon (lambda (lst)
+            (mapcar (lambda (cur)
+                      (list (car lst) cur))
+                    (cdr lst)))
+          positions))
+
+(defun find-minimum-snake-distances-between-pairs (positions)
+  (mapcar (lambda (lst)
+            (apply #'minimum-snake-distance lst))
+          (pair-positions positions)))
+
+(defun find-minimum-snake-distances-between-galaxies (map)
   (->> (find-galaxy-positions map)
        (reverse)
-       (mapcon (lambda (lst)
-                 (mapcar (lambda (cur)
-                           (minimum-snake-distance (car lst) cur))
-                         (cdr lst))))
-       (lib:sum)))
+       (find-minimum-snake-distances-between-pairs)))
+
+(defun part1 (input)
+  (let ((map (build-map input)))
+    (lib:sum (find-minimum-snake-distances-between-galaxies map))))
+
+(defun between-col (x p1 p2)
+  (destructuring-bind (x1 _y1) p1
+    (destructuring-bind (x2 _y2) p2
+      (or (and (< x1 x)
+               (> x2 x))
+          (and (< x2 x)
+               (> x1 x))))))
+
+(defun between-row (y p1 p2)
+  (destructuring-bind (_x1 y1) p1
+    (destructuring-bind (_x2 y2) p2
+      (or (and (< y1 y)
+               (> y2 y))
+          (and (< y2 y)
+               (> y1 y))))))
+
+(defun number-of-intersects-between-positions (lst pair-of-positions predicate)
+  (destructuring-bind (p1 p2) pair-of-positions
+    (loop for e in lst
+          count
+          (funcall predicate e p1 p2))))
+
+(defun number-of-columns-pair-between (columns pair)
+  (number-of-intersects-between-positions columns pair #'between-col))
+
+(defun number-of-rows-pair-between (rows pair)
+  (number-of-intersects-between-positions rows pair #'between-row))
+
+(defun adjust-for-universe-expansion (n)
+  (* *universe-expansion-size* n))
+
+(defun part2 (input)
+   (let* ((lines (load-universe-from-file input))
+          (map (convert-universe-to-2d-array lines))
+          (distances (find-minimum-snake-distances-between-galaxies map))
+          (galaxy-pairs (pair-positions (find-galaxy-positions map)))
+          (empty-columns (find-empty-columns lines))
+          (empty-rows (find-empty-rows lines))
+          (col-adjustment (mapcar (lambda (pair)
+                                    (number-of-columns-pair-between empty-columns
+                                                                    pair))
+                                  galaxy-pairs))
+          (row-adjustment (mapcar (lambda (pair)
+                                    (number-of-rows-pair-between empty-rows
+                                                                    pair))
+                                  galaxy-pairs)))
+     (+ (reduce #'+ col-adjustment :key #'adjust-for-universe-expansion)
+        (reduce #'+ row-adjustment :key #'adjust-for-universe-expansion)
+        (lib:sum distances)
+
+        ;; Adjust for double counting the step
+        (reduce #'+ col-adjustment :key (lambda (n) (* -1 n)))
+        (reduce #'+ row-adjustment :key (lambda (n) (* -1 n))))))
+
+
+(let ((*universe-expansion-size* 1000000))
+  (part2 *input*))
+
+ ; => 550358864332 (40 bits, #x8023F1D1CC)
