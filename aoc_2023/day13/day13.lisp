5957d30b74bdd8db7d505c2ab7757036b72c49cc
Ninju <alex.watt@rvu.co.uk>
2023-12-13 08:17:21 +0000

    Day 13 - Part 2 - solved

--- aoc_2023/day13/day13.lisp
+++ aoc_2023/day13/day13.lisp
@@ -31,6 +31,18 @@
 (defun rock-p (c) (char= c +rock-char+))
 (defun ash-p (c) (char= c +ash-char+))
 
+(defun difference (list-1 list-2)
+  (loop for l1 in list-1
+        and l2 in list-2
+        when (not (equal l1 l2))
+          count 1))
+
+(defun seq-difference (seq-1 seq-2)
+  (loop for l1 across seq-1
+        and l2 across seq-2
+        when (not (equal l1 l2))
+          count 1))
+
 (defun parse-rocks-file (filename) (lib:read-file-line-groups filename))
 
 (defun get-column (map x)
@@ -39,75 +51,107 @@
           collect
           (aref map y x))))
 
-(defun find-candidate-mirror-horizontal (map)
-  (loop for p in (lib:consecutive map)
-        and idx from 0
-        when (destructuring-bind (a b) p (equal a b))
-          collect idx))
-
-(defun find-candidate-mirror-vertical (map)
+(defun find-candidate-mirror-vertical (map &key (test #'equal))
   (let ((as-2d-array (lib:lists->2d-array map)))
     (destructuring-bind (h w) (array-dimensions as-2d-array)
       (loop for x from 0 below (- w 1)
-            when (equal
+            when (funcall test
                   (get-column as-2d-array x)
                   (get-column as-2d-array (+ 1 x)))
             collect
             x))))
 
-(defun check-mirror-horizontal (map y)
-  (block outer
-    (loop for j from y downto 0
-          do
-             (let ((j2 (+ (- y j) 1 y)))
-               (if (>= j2 (length map))
-                   (return-from outer T)
-                   (let ((lhs (elt map j))
-                         (rhs (elt map j2)))
-                     (if (not (equal lhs rhs))
-                         (return-from outer NIL)))))
-          )
-    T))
+(defun find-candidate-mirror-horizontal (map &key (test #'equal))
+  (loop for p in (lib:consecutive map)
+        and idx from 0
+        when (destructuring-bind (a b) p (funcall test a b))
+          collect idx))
 
-(defun check-mirror-vertical (map x)
-  (block outer
-    (let ((as-2d-array (lib:lists->2d-array map))
-          (width (reduce #'min map :key #'length)))
-      (destructuring-bind (h w) (array-dimensions as-2d-array)
-        (loop for i from x downto 0
-              do
-                 (let ((i2 (+ (- x i) 1 x)))
-                   (if (>= i2 width)
-                       (return-from outer T)
-                       (let ((lhs (get-column as-2d-array i))
-                             (rhs (get-column as-2d-array i2)))
-                         (if (not (equal lhs rhs))
-                             (return-from outer NIL))))))))
-    T))
+(defun check-mirror-horizontal (map y &key (required-mismatches 0)
+                                        (test #'equal))
+  (let ((mismatches 0))
+    (block outer
+      (loop for j from y downto 0
+            do
+               (let ((j2 (+ (- y j) 1 y)))
+                 (if (>= j2 (length map))
+                     (return-from outer (= mismatches required-mismatches))
+                     (let ((lhs (elt map j))
+                           (rhs (elt map j2)))
+                       (if (not (funcall test lhs rhs))
+                           (return-from outer NIL)
+                           (progn
+                             (incf mismatches (seq-difference lhs rhs))
+                             (if (> mismatches required-mismatches)
+                                 (return-from outer NIL))))))))
+      (= mismatches required-mismatches))))
 
-(defun find-mirrors (maps)
-  (loop for map in maps
-        collect
-        (block continue
-          (let ((candidate-horizontals (find-candidate-mirror-horizontal map)))
-            (progn
-              (loop for candidate in candidate-horizontals
-                    do
-                       (if (check-mirror-horizontal map candidate)
-                           (return-from continue (list :horizontal candidate))))
+(defun check-mirror-vertical (map x &key (required-mismatches 0)
+                                      (test #'equal))
+  (let ((mismatches 0))
+    (block outer
+      (let ((as-2d-array (lib:lists->2d-array map))
+            (width (reduce #'min map :key #'length)))
+        (destructuring-bind (h w) (array-dimensions as-2d-array)
+          (loop for i from x downto 0
+                do
+                   (let ((i2 (+ (- x i) 1 x)))
+                     (if (>= i2 width)
+                         (return-from outer (= mismatches required-mismatches))
+                         (let ((lhs (get-column as-2d-array i))
+                               (rhs (get-column as-2d-array i2)))
+                           (if (not (funcall test lhs rhs))
+                               (return-from outer NIL)
+                               (progn
+                                 (incf mismatches (difference lhs rhs))
+                                 (if (> mismatches required-mismatches)
+                                     (return-from outer NIL))))))))))
+      (= mismatches required-mismatches))))
 
-              (let ((candidate-verticals (find-candidate-mirror-vertical map)))
+(defun find-mirrors (maps &key (allow-smudge NIL))
+  (let ((test-fn-rows (if (not allow-smudge)
+                          #'equal
+                          (lambda (lhs rhs) (< (seq-difference lhs rhs)
+                                               2))))
+
+        (test-fn-cols (if (not allow-smudge)
+                          #'equal
+                          (lambda (lhs rhs) (< (difference lhs rhs)
+                                               2))))
+
+        (smudge (if allow-smudge 1 0)))
+
+    (loop for map in maps
+          collect
+          (block continue
+            (let ((candidate-horizontals
+                    (find-candidate-mirror-horizontal map :test test-fn-rows)))
+              (progn
+                (loop for candidate in candidate-horizontals
+                      do
+                         (if (check-mirror-horizontal map candidate
+                                                      :required-mismatches smudge
+                                                      :test test-fn-rows)
+                             (return-from continue (list :horizontal candidate))))
+
+              (let ((candidate-verticals (find-candidate-mirror-vertical
+                                          map
+                                          :test test-fn-cols)))
                 (loop for candidate in candidate-verticals
                       do
-                         (if (check-mirror-vertical map candidate)
-                             (return-from continue (list :vertical candidate))))))))))
+                         (if (check-mirror-vertical map candidate
+                                                    :required-mismatches smudge
+                                                    :test test-fn-cols)
+                             (return-from continue (list :vertical candidate)))))))))))
 
 (let ((n 0))
-  (loop for mirror in (find-mirrors (parse-rocks-file (get-inputs-pathname "example.txt")))
+  (loop for mirror in (find-mirrors (parse-rocks-file (get-inputs-pathname "input.txt"))
+                                    :allow-smudge T)
         do
            (if (not mirror)
                (error "MISING MIRROR!")
                (setf n (+ n
                           (if (equal (car mirror) :horizontal)
-                              (prog1 (* 100 (cadr mirror)) (print mirror) (print (cadr mirror)))
-                              (cadr mirror))))
+                              (prog1 (* 100 (+ 1 (cadr mirror))) (print mirror) (print (cadr mirror)))
+                              (+ 1 (cadr mirror)))))))
+  n)
