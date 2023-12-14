f4bd26ad8b92e49bb6f9f29af7293d869125ff5b
Ninju <alex.watt@rvu.co.uk>
2023-12-14 06:01:46 +0000

    STASH: 2023 Day 12 - Part 2

--- aoc_2023/day12/day12.lisp
+++ aoc_2023/day12/day12.lisp
@@ -29,15 +29,17 @@
 (defparameter +unknown-spring-char+ #\?)
 
 (defparameter *whitespace-regex* (ppcre:create-scanner "\\s+"))
-(defparameter *count-separator-regex* "\\s*,\\s*")
+(defparameter *count-separator-regex* (ppcre:create-scanner "\\s*,\\s*"))
+(defparameter *leading-damages-regex* (ppcre:create-scanner "^#+"))
 
 (defparameter *operational-spring-regex* (ppcre:create-scanner "\\.+"))
 (defparameter *damaged-spring-regex* (ppcre:create-scanner "#+"))
-(defparameter *unknown-spring-regex* (ppcre:create-scanner "\\?+"))
+(defparameter *unknown-springs-regex* (ppcre:create-scanner "\\?+"))
+(defparameter *unknown-spring-regex* (ppcre:create-scanner "\\?"))
 (defparameter *spring-regex* (ppcre:create-scanner "[^\.]+"))
 
 (defun unknown-components (spring)
-  (ppcre:all-matches-as-strings *unknown-spring-regex* spring))
+  (ppcre:all-matches-as-strings *unknown-springs-regex* spring))
 
 (defun operational-components (spring)
   (ppcre:all-matches-as-strings *operational-spring-regex* spring))
@@ -97,7 +99,6 @@
                   bit-map
                   unknown-positions)))
 
-
 (defun combine-bit-maps-with-unknown-positions (bit-maps unknown-positions)
   (->> bit-maps
        (mapcar (lambda (bmap)
@@ -142,8 +143,99 @@
   (->> (lib:read-file-lines filename)
        (mapcar #'parse-input-line)))
 
+(defun repeat (times lst)
+  (let ((result '()))
+    (dotimes (n times)
+      (setf result (append lst result)))
+    result))
+
+(defun copies (times e)
+  (let ((result '()))
+    (dotimes (n times)
+      (setf result (cons e result)))
+    result))
+
+(defun explode-input (input)
+  (destructuring-bind (spring counts) input
+    (list (format nil "~{~A~^?.~}" (copies 5 spring))
+          (repeat 5   counts))))
+
+(defun strip-leading-damages (spring counts)
+  (let ((leading-damages (remove nil
+                                 (multiple-value-bind (start end)
+                                     (ppcre:scan *leading-damages-regex* spring)
+                                   (list start end)))))
+    (if (not leading-damages)
+        (list spring counts)
+        (destructuring-bind (start end) leading-damages
+          (let ((new-first-count (- (car counts) end)))
+            (list (subseq spring end)
+                  (if (= 0 new-first-count)
+                      (cdr counts)
+                      (cons (-  (car counts) end)
+                            (cdr counts)))))))))
+
+;; (defmacro delay (form)
+;;   `(lambda () ,form))
+
+;; (defun force (thunk) (funcall thunk))
+
+;; (defun get-left (tree)
+;;   (force (cadr tree)))
+
+;; (get-left (build-damaged-spring-search-tree "????.######..#####."))
+
+;; (defmacro make-node (val left right)
+;;   `(list ,val (delay ,left) (delay ,right)))
+
+;; (defun build-damaged-spring-search-tree (spring)
+;;   (format t "Next arg is: ~a" spring))
+
+;; (defun build-damaged-spring-search-tree (spring)
+;;   (multiple-value-bind (start end) (ppcre:scan *unknown-spring-regex* spring)
+;;     (if (not start) ;; no more decision points
+;;         (make-node spring NIL NIL)
+;;         (make-node (subseq spring 0 start)
+
+;;                    (build-damaged-spring-search-tree
+;;                     (concatenate 'string "#" (subseq spring end)))
+
+;;                    (build-damaged-spring-search-tree
+;;                     (concatenate 'string "." (subseq spring end)))))))
+
+;; (defun search-damaged-spring-tree (tree counts)
+;;   )
+
+(defun empty-p (str)
+  (= 0 (length str)))
+
+(defun walk-damaged-spring (spring counts previous-char-did-consume)
+  (if (empty-p spring)
+      spring
+      (let ((current-char (elt spring 0)))
+        (if (char= +unknown-spring-char+)
+            ;; make choice and branch
+            (if (char= +operational-spring-char+)
+                (if (and previous-char-did-consume
+                         (not (= 0 (car counts))))
+                    NIL ;; invalid situation; we need to consume more via # chars
+                    (walk-damaged-spring (subseq spring 1)
+                                         counts
+                                         NIL)
+  (multiple-value-bind (start end) (ppcre:scan *unknown-spring-regex* spring)
+    (if (not start) ;; no more decision points
+        (arrangement-satisfies-counts-p spring counts)
+        (let* ((everything-up-to-next-unknown (subseq spring 0 start))
+               (everything-after-next-unknown (subseq spring (+ 1 start)))
+               (new-counts (consume-counts everything-up-to-next-unknown counts)))
+          (if (not new-counts)
+              NIL ;; not a valid spring
+              (walk-damaged-spring
+               (concatenate 'string "#" everything-after-next-unknown)
+
+
 (let ((result 0))
-  (loop for input in (part1 (get-pathname "input.txt"))
+  (loop for input in (part1 (get-pathname "example.txt"))
         do
            (destructuring-bind (spring counts) input
              (setf result (+ result (arrangements-n spring counts)))))
