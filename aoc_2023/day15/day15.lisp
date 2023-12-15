ab10de87228f5be98b3aa8a100fc0dc8dece073c
Ninju <alex.watt@rvu.co.uk>
2023-12-15 06:43:50 +0000

    2023 - Day 15 - Solved

--- aoc_2023/day15/day15.lisp
+++ aoc_2023/day15/day15.lisp
@@ -1,8 +1,6 @@
 (in-package :aoc_2023)
 
-(ql:quickload :split-sequence)
 (ql:quickload :cl-ppcre)
-(ql:quickload :alexandria)
 (ql:quickload :arrows)
 
 (use-package :arrows)
@@ -27,11 +25,83 @@
 (defun hash (string)
   (reduce #'step-current-value string :initial-value 0))
 
-(hash "rn=1")
+(defparameter *boxes* (make-hash-table :size 256))
 
-(let ((result 0))
-  (loop for line in (lib:read-file-lines (get-inputs-pathname "input.txt"))
-        do
-           (reduce #'+ (ppcre:split "," )
-                   :key #'hash
-                   :initial-value 0)
+(defun clear-boxes ()
+  (clrhash *boxes*))
+
+(defun lookup-in-box (str)
+  (let ((box (gethash (hash str) *boxes*)))
+    (assoc str box :test #'equal)))
+
+(defun overwrite-box (box-number value)
+  (setf (gethash box-number *boxes*) value))
+
+(defun store-in-box (box-number str focal-length)
+  (let ((current-box (gethash box-number *boxes*)))
+    (if (assoc str current-box :test #'equal)
+        (rplacd (assoc str current-box :test #'equal)
+                focal-length)
+        (setf (gethash box-number *boxes*)
+              (cons (cons str focal-length) current-box)))))
+
+
+(defun remove-in-box (str)
+  (let* ((box-number (hash str))
+         (box (gethash box-number *boxes*)))
+    (let ((new-val (remove-from-alist str box :test #'equal)))
+      (overwrite-box box-number new-val))))
+
+(defun remove-from-alist (item alist &key (test #'eql))
+  (remove (assoc item alist :test test)
+          alist
+          :test test))
+
+(defun process-removal (string)
+  (multiple-value-bind (start end) (ppcre:scan "-" string)
+    (let ((label (subseq string 0 start)))
+      (remove-in-box label))))
+
+
+(defun process-addition (string)
+  (destructuring-bind (label focal-length) (ppcre:all-matches-as-strings "[^=]+"
+                                                                         string)
+    (let* ((box-number (hash label)))
+      (store-in-box box-number label (parse-integer focal-length)))))
+
+(defun process-command (string)
+  (cond
+    ((ppcre:scan "-" string) (process-removal string))
+    (t                       (process-addition string))))
+
+(defun get-input-line (filename)
+  (car
+   (lib:read-file-lines (get-inputs-pathname filename))))
+
+(defun split-file-line-into-commands (line)
+  (ppcre:split "," line))
+
+(defun focusing-power (focal-length box-number slot-number)
+  (-> box-number
+      (+ 1)
+      (* slot-number)
+      (* focal-length)))
+
+(defun part2 (filename)
+  (clear-boxes)
+
+  (->> (get-input-line filename)
+       (split-file-line-into-commands)
+       (mapcar #'process-command))
+
+  (loop for box-number from 0 below 256
+        sum
+        (let* ((box (gethash box-number *boxes*))
+               (num-lenses (length box)))
+          (loop for lens in box
+                and idx from 0
+                sum
+                (let ((slot-number (- num-lenses idx)))
+                  (focusing-power (cdr lens) box-number slot-number))))))
+
+(part2 "input.txt")
