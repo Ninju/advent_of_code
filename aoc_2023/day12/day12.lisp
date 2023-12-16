a6a96faef63a8b44bdc70090db7835d63b5a5f53
Ninju <alex.watt@rvu.co.uk>
2023-12-16 05:43:11 +0000

    STASH: 2023 Day 12

--- aoc_2023/day12/day12.lisp
+++ aoc_2023/day12/day12.lisp
@@ -1,5 +1,6 @@
 (in-package :aoc_2023)
 
+(ql:quickload :lazy)
 (ql:quickload :split-sequence)
 (ql:quickload :cl-permutation)
 (ql:quickload :cl-ppcre)
@@ -7,16 +8,17 @@
 (ql:quickload :arrows)
 
 (use-package :arrows)
+(use-package :lazy)
 
 (defparameter *day-number* 12)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun get-pathname (filename)
+  (defun get-inputs-pathname (filename)
     (arrows:-> filename
                (merge-pathnames (format nil "day~d/inputs/" *day-number*))
                (merge-pathnames #p"/home/alex/src/workspace/advent_of_code/aoc_2023/"))))
 
-(defparameter *input* (get-pathname "input.txt"))
+(defparameter *input* (get-inputs-pathname "input.txt"))
 
 (defun note (&rest args) )
 (defun not-implemented (&optional description &rest body)
@@ -140,7 +142,7 @@
          (list spring (parse-counts-string counts-str))))
 
 (defun part1 (filename)
-  (->> (lib:read-file-lines filename)
+  (->> (lib:read-file-lines (get-inputs-pathname filename))
        (mapcar #'parse-input-line)))
 
 (defun repeat (times lst)
@@ -175,68 +177,273 @@
                       (cons (-  (car counts) end)
                             (cdr counts)))))))))
 
-;; (defmacro delay (form)
-;;   `(lambda () ,form))
+(defun get-left (tree)
+  (lazy-value (cadr tree)))
 
-;; (defun force (thunk) (funcall thunk))
+(defun get-right (tree)
+  (lazy-value (caddr tree)))
 
-;; (defun get-left (tree)
-;;   (force (cadr tree)))
+(defmacro make-node (val left right)
+  `(list ,val (lazy ,left) (lazy ,right)))
 
-;; (get-left (build-damaged-spring-search-tree "????.######..#####."))
+(defun make-node-fn (val left right)
+  (list val left right))
 
-;; (defmacro make-node (val left right)
-;;   `(list ,val (delay ,left) (delay ,right)))
 
-;; (defun build-damaged-spring-search-tree (spring)
-;;   (format t "Next arg is: ~a" spring))
-
-;; (defun build-damaged-spring-search-tree (spring)
-;;   (multiple-value-bind (start end) (ppcre:scan *unknown-spring-regex* spring)
-;;     (if (not start) ;; no more decision points
-;;         (make-node spring NIL NIL)
-;;         (make-node (subseq spring 0 start)
-
-;;                    (build-damaged-spring-search-tree
-;;                     (concatenate 'string "#" (subseq spring end)))
-
-;;                    (build-damaged-spring-search-tree
-;;                     (concatenate 'string "." (subseq spring end)))))))
-
-;; (defun search-damaged-spring-tree (tree counts)
-;;   )
-
-(defun empty-p (str)
-  (= 0 (length str)))
-
-(defun walk-damaged-spring (spring counts previous-char-did-consume)
-  (if (empty-p spring)
-      spring
-      (let ((current-char (elt spring 0)))
-        (if (char= +unknown-spring-char+)
-            ;; make choice and branch
-            (if (char= +operational-spring-char+)
-                (if (and previous-char-did-consume
-                         (not (= 0 (car counts))))
-                    NIL ;; invalid situation; we need to consume more via # chars
-                    (walk-damaged-spring (subseq spring 1)
-                                         counts
-                                         NIL)
+(defun build-damaged-spring-search-tree (spring)
   (multiple-value-bind (start end) (ppcre:scan *unknown-spring-regex* spring)
     (if (not start) ;; no more decision points
-        (arrangement-satisfies-counts-p spring counts)
-        (let* ((everything-up-to-next-unknown (subseq spring 0 start))
-               (everything-after-next-unknown (subseq spring (+ 1 start)))
-               (new-counts (consume-counts everything-up-to-next-unknown counts)))
-          (if (not new-counts)
-              NIL ;; not a valid spring
-              (walk-damaged-spring
-               (concatenate 'string "#" everything-after-next-unknown)
+        (make-node spring NIL NIL)
+        (make-node (subseq spring 0 start)
+
+                   (build-damaged-spring-search-tree
+                    (concatenate 'string "#" (subseq spring end)))
+
+                   (build-damaged-spring-search-tree
+                    (concatenate 'string "." (subseq spring end)))))))
+
+(defparameter *rec* '())
+(defun rec (x) (progn (push (node-value x) *rec*) x))
+
+(setf *rec* '())
+(->>
+     (build-damaged-spring-search-tree "#?.?#..")
+     (rec)
+     (get-child :left)
+     (rec)
+     (get-child :left)
+     (rec)
+     (get-child :right)
+     (rec))
+
+(defun build-tree (input)
+  (destructuring-bind (spring counts) input
+      (list (build-damaged-spring-search-tree spring)
+            counts)))
+
+(defun get-child (child-id node)
+  (cond
+    ((eql child-id :left) (get-left node))
+    ((eql child-id :right) (get-right node))))
+
+(defun get-child-with-leading-char (c node)
+  (cond
+    ((char= c +damaged-spring-char+) (get-left node))
+    ((char= c +operational-spring-char+) (get-right node))))
+
+(define-condition invalid-arrangement-due-to-mismatched-counts (error)
+  ((arrangement :initarg arrangement)
+   (counts :initarg counts)))
+
+(make-condition 'invalid-arrangement-due-to-mismatched-counts)
+
+(defun consume-counts (spring counts)
+  (if (not (ppcre:scan *damaged-spring-regex* spring))
+      (values (list spring counts) 0 T)
+      (let ((count (car counts)))
+
+        (multiple-value-bind (start end) (ppcre:scan *damaged-spring-regex*
+                                                     spring)
+
+          (let* ((consumed (- end start))
+                 (unconsumed-spring (subseq spring end)))
+            (cond
+              ;; we've consumed more than allowed (cluster of #s > count)
+              ;; OR
+              ;; we've not consumed the entire count and the following char
+              ;; is not a #..
+              ((or (> consumed count) (and (< consumed count)
+                                           (> (length unconsumed-spring) 0)))
+
+               (error 'invalid-arrangement-due-to-mismatched-counts
+                      :counts counts
+                      :arrangement spring))
+
+              ((< consumed count) (values (list unconsumed-spring
+                                                (cons (- count consumed)
+                                                      (cdr counts)))
+                                          consumed
+                                          NIL))
+
+              ((and (= consumed count)
+                    (not (cdr counts))) (values (list unconsumed-spring NIL)
+                                                consumed
+                                                T))
+
+              ((= consumed count) (consume-counts unconsumed-spring
+                                                  (cdr counts)))))))))
+
+(defun node-value (node)
+  (car node))
+
+(define-condition result-found (condition)
+  ((result :initarg :result
+           :reader get-result)))
+
+(make-condition 'result-found)
+
+(defun contains-damaged-p (spring)
+  (ppcre:scan *damaged-spring-regex* spring))
+
+;; Choose a branch and search that. Catch results, then float the result up to
+;; top-level using `signal` and also branch right so we can search that side of
+;; the tree
+(defun make-decision (node counts current-arrangement)
+  (let ((decisions (list +damaged-spring-char+)))
+
+    (format t "~&++++ Making decision to go to '#'")
+    (handler-case
+        (branch-into +damaged-spring-char+
+                     node
+                     counts
+                     current-arrangement)
+
+      (invalid-arrangement-due-to-mismatched-counts ()
+        (progn (format t "~&---- Stepping back to decision point (invalid counts)")
+
+               (if (not (find +operational-spring-char+ decisions))
+                   (progn (setf decisions (cons +operational-spring-char+ decisions))
+                          (branch-into +operational-spring-char+
+                                       node
+                                       counts
+                                       current-arrangement)))))
+
+      (result-found (c) (progn
+                          (format t "~&---- Stepping back to decision point: result found = ~S" (get-result c))
+                          (signal c)
+                          (if (not (find +operational-spring-char+ decisions))
+                              (progn
+                                     (setf decisions (cons +operational-spring-char+ decisions))
+                                     (branch-into +operational-spring-char+
+                                                  node
+                                                  counts
+                                                  current-arrangement))
+                              (format t "~&---- ALL DECISIONS MADE")))))))
+
+(defun mid-level
+    ()
+  (handler-bind ((result-found (lambda (c)
+                                 (print (get-result c)))))
+    (loop for n from 0 upto 100
+          do
+             (if (= 0 (mod n 7))
+                 (progn (signal 'result-found :result n))))))
+
+(defun top-level ()
+
+  (let ((results '()))
+    (handler-bind ((result-found (lambda (c)
+                                   (push (get-result c) results))))
+      (mid-level))
+    results))
+
+(top-level)
+
+(defun branch-into (char node counts current-arrangement)
+  (let ((child (get-child-with-leading-char char node))
+        (new-arrangement (format nil
+                                 "~A~A"
+                                 current-arrangement
+                                 (node-value node))))
+    (if (not child)
+        (if (not counts)
+            (signal 'result-found :result new-arrangement)
+            NIL)
+        (progn (step-through-node child
+                           counts
+                           new-arrangement)
+               (format t "~&    stepping through into child: ~c" char)))))
+
+(defun step-through-node (node counts current-arrangement)
+  (format t "~&'~a|~a' ~a" current-arrangement (node-value node) counts)
+
+  (let ((current-value (node-value node)))
+    (cond
+      ((not counts) (if (contains-damaged-p current-value)
+                        NIL
+                        (branch-into +operational-spring-char+
+                                     node
+                                     counts
+                                     current-arrangement)))
+
+      ((not
+        (contains-damaged-p current-value)) (make-decision node
+                                                           counts
+                                                           current-arrangement))
+
+      (t (handler-case
+             (multiple-value-bind
+                   (result num-consumed fully-consumed) (consume-counts current-value
+                                                                        counts)
+               (destructuring-bind (unconsumed-spring new-counts) result
+
+                   (cond
+                     ((string= "" unconsumed-spring) (if fully-consumed
+                                                          (branch-into +operational-spring-char+
+                                                                       node
+                                                                       new-counts
+                                                                       current-arrangement)
+                                                          (branch-into +damaged-spring-char+
+                                                              node
+                                                              new-counts
+                                                              current-arrangement)))
 
 
-(let ((result 0))
-  (loop for input in (part1 (get-pathname "example.txt"))
-        do
-           (destructuring-bind (spring counts) input
-             (setf result (+ result (arrangements-n spring counts)))))
-  result)
+                     (fully-consumed (make-decision node
+                                                    new-counts
+                                                    current-arrangement))
+
+                     ((not fully-consumed)       (error "consume counts should have raised an error")))))
+
+           ;; Condition handlers
+           (invalid-arrangement-due-to-mismatched-counts () NIL))))))
+
+(defun search-all-arrangements (root-node counts)
+  (format t "~&Searching root-node ~A with counts ~a~%" root-node counts)
+
+  (let ((arrangements '()))
+    (handler-bind ((result-found (lambda (c3)
+                                   (push (get-result c3) arrangements))))
+      (step-through-node root-node counts ""))
+    arrangements))
+
+(defun arrangements-for-input (input)
+  (destructuring-bind (tree counts) input
+    (search-all-arrangements tree counts)))
+
+ ; => ("???.###" (1 1 3))
+
+;; (let ((arrangements (->> (part1 "example.txt")
+;;                          ;; (mapcar #'explode-input)
+;;                          (mapcar #'build-tree)
+;;                          (mapcar #'arrangements-for-input))))
+;;   arrangements)
+
+;; (search-all-arrangements (build-damaged-spring-search-tree "?") '(1)) ;; '("..")
+
+;; (step-through-node (build-damaged-spring-search-tree "?") '(1) "")
+
+(search-all-arrangements (build-damaged-spring-search-tree "??????") '(1 1)) ;; '("..")
+
+;; (node-value (build-damaged-spring-search-tree "?")
+
+;; (defparameter *decisions* (make-hash-table :size 10000
+;;                                            :test #'equal))
+
+;; (defun search (string cursor decisions counts consuming-count current-arrangement)
+;;   (let ((current-char (elt string cursor)))
+;;     (cond
+;;       (
+;;        ;; Just finished consuming the count
+;;        (and (char= current-char +unknown-spring-char+)
+;;             (= 0 consuming-count))
+;; (if (and consuming-count
+;;                                                            (> consuming-count 0))
+;;                                                       (progn (push (elt decisions cursor)
+;;                                                                    +damaged-spring-char+)
+;;                                                              (search string
+;;                                                                      cursor
+;;                                                                      decisions
+;;                                                                      counts
+;;                                                                      (- 1 consuming-count)
+;;                                                                      (format nil "~A~A" current-arrangement +damaged-spring-char+)))
