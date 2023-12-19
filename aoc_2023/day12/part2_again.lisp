c86885fad4d8a34df9e3b63b4a72ec0eba2c583e
Ninju <alex.watt@rvu.co.uk>
2023-12-19 22:12:34 +0000

    STASH: Day 19 - Part 2

---
+++ aoc_2023/day12/part2_again.lisp
@@ -0,0 +1,249 @@
+(in-package :aoc_2023)
+
+(ql:quickload :cl-ppcre)
+(ql:quickload :arrows)
+
+(use-package :arrows)
+
+(declaim (optimize (debug 3)))
+
+(defparameter *day-number* 12)
+(defparameter *count-separator-regex* (ppcre:create-scanner "\\s*,\\s*"))
+(defparameter *whitespace-regex* (ppcre:create-scanner "\\s+"))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun get-inputs-pathname (filename)
+    (arrows:-> filename
+               (merge-pathnames (format nil "day~d/inputs/" *day-number*))
+               (merge-pathnames #p"/home/alex/src/workspace/advent_of_code/aoc_2023/"))))
+
+(defun create-unvisited (next-char str cursor counts cur)
+  (list
+   (format nil "~A~A" cur next-char)
+   (+ 1 cursor)
+   (consume-counts-with-char counts next-char)))
+
+(defun consume-counts (counts amount)
+  (if (> amount (car counts))
+      (error "Cannot consume counts by that much!")
+      (cons (- (car counts) amount)
+            (cdr counts))))
+
+(defun consume-counts-with-char (counts char)
+  (if (char= #\# char)
+      (consume-counts counts 1)
+      counts))
+
+(defun counts-remaining-p (counts)
+  (> (count-if (lambda (n) (> n 0)) counts) 0))
+
+(defun branch-search-arrangements (str cursor counts unvisited cur acc)
+  ;; (format t "~&BRANCHING: ~a (~d)" cur cursor)
+  (let ((new-unvisited (create-unvisited #\. str cursor counts cur)))
+
+      (continue-search-arrangements #\#
+                                    str
+                                    cursor
+                                    (consume-counts counts 1)
+                                    (cons new-unvisited unvisited)
+                                    cur
+                                    acc)))
+
+
+(defun continue-search-arrangements (new-char str cursor new-counts unvisited cur acc)
+  (search-arrangements str
+                       (+ 1 cursor) ;; already know the next char and have adjusted the counts accordingly
+                       new-counts
+                       unvisited
+                       (format nil "~A~A" cur new-char)
+                       acc))
+
+(defun search-from-next-unvisited (str unvisited acc)
+  (if (not unvisited)
+      (progn ;; (format t "~&DONE")
+             acc)
+      (destructuring-bind (next-str next-cursor next-counts) (car unvisited)
+
+        (progn
+          ;; (format t "~&UNVISITED: ~a (~d)" next-str next-cursor)
+
+          (search-arrangements str
+                               next-cursor
+                               next-counts
+                               (cdr unvisited)
+                               next-str
+                               acc)))))
+
+(defun counts-just-fully-consumed-p (counts)
+  (= 0 (car counts)))
+
+(defun counts-fully-consumed-p (counts)
+  (not counts))
+
+(defun counts-empty-p (counts)
+  (not counts))
+
+(defun safe-previous-char (str cursor)
+  (if (> cursor 0)
+      (elt str (- cursor 1))
+      #\+))
+
+(defun damaged-char-p (str cursor)
+  (char= #\# (elt str cursor)))
+
+(defun unknown-char-p (str cursor)
+  (char= #\? (elt str cursor)))
+
+(defun empty-char-p (str cursor)
+  (char= #\. (elt str cursor)))
+
+(defun search-arrangements (str cursor counts unvisited cur acc)
+  (cond
+    ;; CHECK: at end of string?
+    ((>= cursor (length str))
+     (if (counts-remaining-p counts)
+         (search-from-next-unvisited str unvisited acc)
+         (search-from-next-unvisited str unvisited (cons cur acc))))
+
+    ;; CHECK: already consumed counts but have more '#'
+    ((and (counts-empty-p counts)
+          (ppcre:scan "#" (subseq str cursor)))
+     (search-from-next-unvisited str unvisited acc))
+
+    ((counts-empty-p counts)
+     (search-from-next-unvisited str
+                                 unvisited
+                                 (cons
+                                  (format nil "~A~A"
+                                          (subseq str 0 cursor)
+                                          (ppcre:regex-replace-all "\\?"
+                                                                   (subseq str cursor)
+                                                                   "."))
+                                  acc)))
+    ((> (lib:sum counts)
+        (or (ppcre:all-matches-as-strings "[\\?#]" (subseq str cursor)) 0))
+     (search-from-next-unvisited str
+                                 unvisited
+                                 acc))
+
+  ;; CHECK: just finished consuming, next char must be a dot '.'
+  ((and (counts-just-fully-consumed-p counts)
+        (damaged-char-p str cursor))
+   (search-from-next-unvisited str unvisited acc))
+
+  ((and (counts-just-fully-consumed-p counts)
+        (or
+         (empty-char-p str cursor)
+         (unknown-char-p str cursor)))
+   (continue-search-arrangements #\. str cursor (cdr counts) unvisited cur acc))
+
+  ;; CHECK: previous char was '#' and we have counts to consume, then current
+  ;;        char must be '#'
+  ((and (counts-remaining-p counts)
+        (char= #\# (safe-previous-char str cursor))
+        (or (damaged-char-p str cursor)
+            (unknown-char-p str cursor)))
+
+   (continue-search-arrangements #\#
+                                 str
+                                 cursor
+                                 (consume-counts counts 1)
+                                 unvisited
+                                 cur
+                                 acc))
+
+  ((and (counts-remaining-p counts)
+        (char= #\# (safe-previous-char str cursor))
+        (empty-char-p str cursor))
+   (search-from-next-unvisited str unvisited acc))
+
+  ((or (damaged-char-p str cursor)
+       (empty-char-p str cursor))
+
+      (continue-search-arrangements (elt str cursor)
+                                    str
+                                    cursor
+                                    (consume-counts-with-char counts
+                                                              (elt str cursor))
+                                    unvisited
+                                    cur
+                                    acc))
+
+     ((unknown-char-p str cursor)
+      (branch-search-arrangements str cursor counts unvisited cur acc))
+
+     (t (error "Non-exhaustive search for string"))))
+
+
+(defun search-all-arrangements (str counts)
+  (search-arrangements str 0 counts '() "" '()))
+
+(defun repeat (times lst)
+  (let ((result '()))
+    (dotimes (n times)
+      (setf result (append lst result)))
+    result))
+
+(defun copies (times e)
+  (let ((result '()))
+    (dotimes (n times)
+      (setf result (cons e result)))
+    result))
+
+(defun explode-input (input)
+  (destructuring-bind (spring counts) input
+    (list (format nil "~{~A~^?.~}" (copies 5 spring))
+          (repeat 5 counts))))
+
+(defun parse-counts-string (str)
+  (mapcar #'parse-integer (ppcre:split *count-separator-regex* str)))
+
+(defun parse-input-line (line)
+       (destructuring-bind (spring counts-str)
+           (ppcre:split *whitespace-regex* line)
+         (list spring (parse-counts-string counts-str))))
+
+(defun load-exploded-input (filename)
+  (->> (lib:read-file-lines (get-inputs-pathname filename))
+       (mapcar #'parse-input-line)
+       (mapcar #'explode-input)))
+
+#+nil
+(let ((SLYNK-STICKERS:*BREAK-ON-STICKERS* (list :after)))
+  (search-all-arrangements "???.###?" '(1 1 3)))
+
+#+nil
+(reduce #'+
+        (load-exploded-input "example.txt")
+        :key (lambda (input)
+               (destructuring-bind (str counts) input
+                   (search-all-arrangements str counts))))
+
+;; Exploded test input
+
+#+nil
+ (search-all-arrangements "???.###?.???.###?.???.###?.???.###?.???.###" '(1 1 3 1 1 3 1 1 3 1 1 3 1 1 3))
+
+#+nil
+ (search-all-arrangements "????" '(1 1))
+
+
+#+nil
+(search-all-arrangements
+ "????.#...#...?.????.#...#...?.????.#...#...?.????.#...#...?.????.#...#..."
+ '(4 1 1 4 1 1 4 1 1 4 1 1 4 1 1))
+
+#+nil
+(let ((SLYNK-STICKERS:*BREAK-ON-STICKERS* (list :after)))
+ (search-all-arrangements "????????" '(1 2)))
+
+ ; (".??..??...?##.?..??..??...?##.?..??..??...?##.?..??..??...?##.?..??..??...?##."
+ ;  (1 1 3 1 1 3 1 1 3 1 1 3 1 1 3))
+ ; ("?#?#?#?#?#?#?#??.?#?#?#?#?#?#?#??.?#?#?#?#?#?#?#??.?#?#?#?#?#?#?#??.?#?#?#?#?#?#?#?"
+ ;  (1 3 1 6 1 3 1 6 1 3 1 6 1 3 1 6 1 3 1 6))
+ ; ("????.#...#...?.????.#...#...?.????.#...#...?.????.#...#...?.????.#...#..."
+ ;  (4 1 1 4 1 1 4 1 1 4 1 1 4 1 1))
+ ; ("????.######..#####.?.????.######..#####.?.????.######..#####.?.????.######..#####.?.????.######..#####."
+ ;  (1 6 5 1 6 5 1 6 5 1 6 5 1 6 5))
+ ; ("?###?????????.?###?????????.?###?????????.?###?????????.?###????????"
+ ;  (3 2 1 3 2 1 3 2 1 3 2 1 3 2 1)))
--- aoc_2023/day19/day19.lisp
+++ aoc_2023/day19/day19.lisp
@@ -1,5 +1,6 @@
 (in-package :aoc_2023)
 
+(ql:quickload :alexandria)
 (ql:quickload :arrows)
 (ql:quickload :cl-ppcre)
 
@@ -15,7 +16,16 @@
         (merge-pathnames (format nil "day~d/inputs/" *day-number*))
         (merge-pathnames #p"/home/alex/src/workspace/advent_of_code/aoc_2023/"))))
 
+(defun alist-update (key alist new-value &rest args)
+  (let ((args-with-key (append args (list :key #'car))))
+    (funcall #'acons key new-value
+             (apply #'remove key alist args-with-key))))
+
 ;; DATA STRUCTURES
+
+(defun make-range (a b) (cons a b))
+(defun start (range) (car range))
+(defun end (range) (cdr range))
 
 (defclass rule (standard-object)
   ((category :initarg :category :reader category)
@@ -58,6 +68,16 @@
           (value)))
 
 ;; END DATA STRUCTURES
+;; DYNAMIC BINDINGS
+
+(defparameter *upper-limit* 4000)
+(defparameter *initial-ranges* (list
+                                (cons "x" (make-range 1 *upper-limit*))
+                                (cons "m" (make-range 1 *upper-limit*))
+                                (cons "a" (make-range 1 *upper-limit*))
+                                (cons "s" (make-range 1 *upper-limit*))))
+
+;; END DYNAMIC BINDINGS
 ;; PARSING
 
 (defun parse-rating (rating)
@@ -104,8 +124,8 @@
 (defun apply-rule (rule rating)
   (let ((rating-value (get-rating-value (category rule) rating)))
     (if (funcall (lookup-operator (operator rule))
-               rating-value
-               (threshold rule))
+                 rating-value
+                 (threshold rule))
         (outcome rule))))
 
 (defun process-rating-through-workflow (rating workflow)
@@ -159,9 +179,324 @@
               (find-accepted-ratings workflows-hashmap ratings)
               :key #'sum-rating-parts))))
 
+(defun restrict-range-to-meet-threshold (current-range operator threshold)
+  (cond
+    ((string= operator ">")
+     (if (> (end current-range) threshold)
+         (make-range (max (start current-range)
+                          (+ threshold 1))
+                     (max (end current-range)
+                          (+ threshold 1)))))
+
+    ((string= operator "<")
+
+     (if (< (start current-range) threshold)
+         (make-range (min (start current-range)
+                          (- threshold 1))
+                     (min (end current-range)
+                          (- threshold 1)))))))
+
+(defun range-for-rule (rule)
+  (cond
+    ((string= (operator rule) ">") (make-range (+ 1 (threshold rule))
+                                               *upper-limit*))
+
+    ((string= (operator rule) "<") (make-range 0
+                                               (- (threshold rule)
+                                                  1)))))
+
+;; RUNE
+;;
+ ; => (1549 . 4000), (4001 . 4000), (838 . 1548)
+(defun stable-range-intersect (range-1 range-2)
+  (cond
+    ((or (> (start range-1) (end range-2))
+         (> (start range-2) (end range-1))) (values NIL range-1 range-2))
+
+    (t
+     (let* ((intersection (make-range (max (start range-1)
+                                           (start range-2))
+                                      (min (end range-1)
+                                           (end range-2))))
+
+            (less-than-range (make-range (min (start range-1)
+                                              (start range-2))
+                                         (- (max (start range-1)
+                                                 (start range-2))
+                                            1)))
+            (more-than-range (if (not (= (end intersection)
+                                         (end range-1)))
+                                 (make-range (+ 1 (min (end range-1)
+                                                       (end range-2)))
+                                             (max (end range-1)
+                                                  (end range-2))))))
+       (values-list
+        (cons intersection
+              (if (< (start range-1) (start range-2))
+                  (list less-than-range more-than-range)
+                  (list more-than-range less-than-range))))))))
+
+(defun apply-rule-to-ranges (rule ranges)
+  (let* ((cat (category rule))
+         (cat-range (cdr (assoc cat ranges :test #'equal))))
+
+    (multiple-value-bind (matched-range lhs-unmatched rhs-unmatched)
+        (range-intersect (range-for-rule rule) cat-range)
+
+      (cons (if matched-range
+                (cons (outcome rule)
+                      (alist-update cat ranges matched-range :test #'equal)))
+
+            (mapcar (lambda (unmatched-range)
+                      (cons NIL (alist-update cat ranges unmatched-range :test #'equal)))
+                    (remove nil (list lhs-unmatched rhs-unmatched)))))))
+
+
+(defun apply-workflow-to-ranges (workflow ranges)
+  (let ((results '())
+        (unmatched-ranges (list ranges))
+        (interim-ranges '()))
+
+    (loop for rule in (rules workflow)
+          do
+             ;; (format t "~3&Processing next rule: ~A ~A ~A => ~A"
+             ;;         (category rule)
+             ;;         (operator rule)
+             ;;         (threshold rule)
+             ;;         (outcome rule))
+             (loop for range in unmatched-ranges
+                   do
+                   (destructuring-bind (matched &rest unmatched)
+                       (apply-rule-to-ranges rule range)
+
+                     (if matched
+                         (push matched results))
+
+                     (if unmatched
+                         (setf interim-ranges
+                               (remove nil
+                                       (append (mapcar #'cdr unmatched)
+                                               interim-ranges))))))
+
+             ;; (format t "~&UNMATCH RANGES = ~A" unmatched-ranges)
+             ;; (format t "~&INTERIM RANGES = ~A" interim-ranges)
+             (setf unmatched-ranges interim-ranges)
+             (setf interim-ranges '())
+
+             (append results matched)
+             ;; (format t "~&INTERIM RANGES = ~A" interim-ranges)
+             )
+
+
+
+    (append results (mapcar (lambda (unmatched-range)
+                                       (cons (default-outcome workflow)
+                                             unmatched-range))
+                                     unmatched-ranges))))
+
+
+(defun load-workflows-hashmap-from-file (filename)
+  (destructuring-bind (workflows ratings) (load-file filename)
+    (declare (ignore ratings))
+    (build-workflow-hashmap workflows)))
+
+(defun step-forward-workflow (entrypoint workflows-hashmap initial-ranges)
+  (let* ((workflow (gethash entrypoint workflows-hashmap))
+         (ranges initial-ranges)
+         (accepted-ranges '()))
+
+    (let ((new-ranges (apply-workflow-to-ranges workflow ranges)))
+
+      (let ((a-ranges (remove-if-not (lambda (e)
+                                       (string= "A" (car e)))
+                                     new-ranges))
+            (other-ranges (remove-if (lambda (e)
+                                       (or (string= "R" (car e))
+                                           (string= "A" (car e))))
+                                     new-ranges)))
+
+        (setf accepted-ranges (append (mapcar #'cdr a-ranges)
+                                      accepted-ranges))
+
+        (list accepted-ranges other-ranges)))))
+
+(defparameter *iter-limit* 10000)
+
+(defun step-forward-workflows-recursively (workflows-hashmap
+                                           queue
+                                           acc-accepted)
+  (if (not queue)
+      (return-from step-forward-workflows-recursively acc-accepted))
+
+  (decf *iter-limit*)
+  (if (< *iter-limit* 0)
+      (progn (setf *iter-limit* 0)
+             (error "Reached iteration limit!")))
+
+  (let* ((current (car queue))
+         (current-workflow-id (car current))
+         (current-ranges (cdr current)))
+
+    (destructuring-bind (accepted others)
+        (step-forward-workflow current-workflow-id
+                               workflows-hashmap
+                               current-ranges)
+
+      (step-forward-workflows-recursively workflows-hashmap
+                                          (append others (cdr queue))
+                                          (append accepted acc-accepted)))))
+
+(defun ranges->distinct-possibilities (ranges)
+  (reduce #'*
+          ranges
+          :key (lambda (range) (- (end range) (start range)))))
+
+(defun condense-range (r1 r2)
+  (range-intersect '(300 . 450) '(200 . 300))
+  )
+
+(defun range-size (r) (- (end r) (start r)))
+
+(defun consolidate-ranges (r1 r2)
+  (loop for c in (list "x" "m" "a" "s")
+        collect
+        (multiple-value-bind (inter lhs rhs) (range-intersect (alexandria:assoc-value r1 c :test #'equal)
+                                                              (alexandria:assoc-value r2 c :test #'equal))
+            (if (not inter)
+                (return nil)
+                (cons c (list inter lhs rhs)))))))))
+
+#+nil
+(consolidate-ranges (caddr *test-accepted-ranges*)
+                    (cadddr *test-accepted-ranges*))
+
+
+
+(defun part2 ()
+  (let ((all-m (loop for accepted in *test-accepted-ranges*
+                     collect (assoc "s" accepted :test #'equal))))
+
+    (remove-duplicates
+     (sort (mapcar #'cdr all-m)
+           #'>
+           :key #'range-size) :test #'equal))
+  ;;
+  ;; (* 4000 4000 3711 4000)
+  ;; (< (* 1337 838 1716 551) 167409079868000)
+  ;; 167409079868000
+  )
+
+#+nil
+(defparameter *test-rule* (parse-rule "s<537:gd,x>2440:R,A"))
+
+#+nil
+(defparameter *test-accepted-ranges* '((("m" 1549 . 4000) ("s" 2771 . 3448) ("x" 0 . 4000) ("a" 0 . 4000))
+                                       (("m" 0 . 1548) ("s" 2771 . 3448) ("x" 0 . 4000) ("a" 0 . 4000))
+                                       (("s" 3449 . 4000) ("x" 0 . 4000) ("m" 0 . 4000) ("a" 0 . 4000))
+                                       (("a" 0 . 1716) ("m" 1801 . 4000) ("s" 1351 . 2770) ("x" 0 . 4000))
+                                       (("a" 0 . 1716) ("m" 0 . 838) ("s" 1351 . 2770) ("x" 0 . 4000))
+                                       (("m" 839 . 1800) ("s" 1351 . 2770) ("x" 0 . 4000) ("a" 0 . 4000))
+                                       (("x" 0 . 2440) ("s" 537 . 1350) ("m" 0 . 2090) ("a" 2006 . 4000))
+                                       (("x" 2663 . 4000) ("a" 0 . 2005) ("s" 0 . 1350) ("m" 0 . 4000))
+                                       (("x" 0 . 1415) ("a" 0 . 2005) ("s" 0 . 1350) ("m" 0 . 4000))
+                                       (("m" 2091 . 4000) ("a" 2006 . 4000) ("s" 0 . 1350) ("x" 0 . 4000))))
+
+#+nil
+(loop for ranges in (step-forward-workflows-recursively (load-workflows-hashmap-from-file "example.txt")
+                                                        (list (cons "in" *initial-ranges*))
+                                                        NIL)
+      sum
+      (ranges->distinct-possibilities (mapcar #'cdr ranges)))
+
+ ; => 188598466743200 (48 bits, #xAB87809D8BA0)
+
+
+#+nil
+(step-forward-workflows-recursively (load-workflows-hashmap-from-file "example.txt")
+                                    (list (cons "in" *initial-ranges*))
+                                    NIL)
+
+#+nil
+(apply-rule-to-ranges *test-rule* *initial-ranges*)
+
+#+nil
+(car (mapcar #'cdr '(("gd" ("s" 0 . 536) ("x" 0 . 4000) ("m" 0 . 4000) ("a" 0 . 4000))
+ (NIL ("s" 537 . 4000) ("x" 0 . 4000) ("m" 0 . 4000) ("a" 0 . 4000)))))
+
+#+nil
+(apply-workflow-to-ranges (parse-workflow "px{a<2006:qkq,m>2090:A,rfg}")
+                          *initial-ranges*)
+
+#+nil
+(let ((workflows-hashmap (load-workflows-hashmap-from-file "example.txt")))
+  (destructuring-bind (accepted others)
+      (step-forward-workflow "in" workflows-hashmap
+                             *initial-ranges*)
+    (loop for other in others
+          appending
+          (step-forward-workflow (car other)
+                                 (load-workflows-hashmap-from-file "example.txt")
+                                 (cdr other)))))
+
+
+#+nil
+(assoc (category *test-rule*) *initial-ranges* :test #'equal)
+
+#+nil
+(range-intersect '(100 . 400) '(50 . 380))
+;; => (50 . 99) (100 . 380) (381 . 400)
+
+#+nil
+(range-intersect '(0 . 4000) '(0 . 2005))
+
+#+nil
+(range-intersect '(0 . 1000) '(2000 . 2005))
+
+#+nil
+(restrict-range-to-meet-threshold '(0 . 4000) "<" 2006)
+
+#+nil
+(limit-value 4000 4000 ">" 1351)
+
+;; (defun range-limiter (ranges rule)
+;;   (let ((ranges (("x" 4000)
+;;                  ("m" 4000)
+;;                  ("a" 4000)
+;;                  ("s" 4000))))
+;;     (range-limiter (("x" 4000)
+;;                     ("m" 4000)
+;;                     ("a" 2005)
+;;                     ("s" 4000))
+;;                    (rule-for "qkq"))
+
+;;     (ACCEPTED (("x" 4000)
+;;                ("m" (- 4000 2089))
+;;                ("a" (- 4000 2005))
+;;                ("s" 4000)))
+
+;;     (range-limiter (("x" 4000)
+;;                     ("m" 4000)
+;;                     ("a" 2005)
+;;                     ("s" 4000))
+;;                    (rule-for "rfg"))))
+
+
+
+#+nil
+(+ (* 2005 2089 4000 4000)
+   (* 1716 4000 4000 4000)
+   (* 4000 * 4000 * 4000 * 4000)
+   (* 538 2440 4000 4000)
+   (* (- 4000 3448) 4000 4000 4000)
+   (* (+ 1415 (- 4000 2662)) 4000 4000 4000)
+   (* "in")
+   (* "qqz")
+   ;; (* 0 "gd")
+   (* (- 4000 839) (* 838 1715 4000 4000)))
+
+
 #+nil
 (part1 "input.txt")
-
 
 #+nil
 (load-file "example.txt")
