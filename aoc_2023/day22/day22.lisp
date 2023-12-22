4823669805c9bdb0ccb25bf86d781855a6b7d0e8
Ninju <alex.watt@rvu.co.uk>
2023-12-22 06:22:03 +0000

    Create brick data structure

--- aoc_2023/day22/day22.lisp
+++ aoc_2023/day22/day22.lisp
@@ -33,7 +33,52 @@
          (progn (setf *iter-limit* 0)
                 (error "Reached iteration limit!")))))
 
-(defun make-brick (lhs rhs) (list lhs rhs))
+(defun make-point-3d (x y z) (list x y z))
+(defun x-coord (pos) (car pos))
+(defun y-coord (pos) (cadr pos))
+(defun z-coord (pos) (caddr pos))
+
+(defstruct (brick (:constructor %make-brick)) left-edge right-edge bounding-rect)
+
+(defun make-brick (lhs rhs)
+  (%make-brick :left-edge lhs
+               :right-edge rhs
+               :bounding-rect (apply #'make-rect
+                                     (if (= (x-coord lhs)
+                                            (x-coord rhs))
+                                         (sort (list lhs rhs)
+                                               #'<
+                                               :key #'y-coord)
+                                         (sort (list lhs rhs)
+                                               #'<
+                                               :key #'x-coord)))))
+
+(defun left-edge (brick)
+  (with-slots (left-edge) brick
+    left-edge))
+
+(defun right-edge (brick)
+  (with-slots (right-edge) brick
+    right-edge))
+
+(defun brick-volume (brick) 0)
+(defun brick-on-ground-p (brick)
+  (or (= *ground-z* (z-coord (left-edge brick)))
+      (= *ground-z* (z-coord (right-edge brick)))))
+(defun min-z-coord (brick)
+  (min (z-coord (left-edge brick))
+       (z-coord (right-edge brick))))
+
+(defun brick-x-coords (brick) (list (x-coord (left-edge brick))
+                                    (x-coord (right-edge brick))))
+
+(defun brick-y-coords (brick) (list (y-coord (left-edge brick))
+                                    (y-coord (right-edge brick))))
+
+(defun add-points-3d (p1 p2)
+  (make-point-3d (+ (x-coord p1) (x-coord p2))
+                 (+ (y-coord p1) (y-coord p2))
+                 (+ (z-coord p1) (z-coord p2))))
 
 (defun parse-coords (coords)
   (mapcar #'parse-integer (ppcre:split "," coords)))
